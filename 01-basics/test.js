0876c6aaa3c3987dd3628078938919924cfe26cf
jsprogrammer15 <151398232+jsprogrammer15@users.noreply.github.com>
2023-11-20 20:10:32 +0000

    config done

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,24 @@
 console.log("abdullah")
+
+const accountId = 124;
+let accountEmail = "ab@gmail.com";
+var pass ="123460"
+accountCity = "karachi"
+
+// accountId =555 = changing the const variable not allowed 
+
+console.log(accountId);
+accountEmail ="hb@google"
+accountCity="lahore";
+pass= 5645;
+let accountState;
+/*
+
+prefer not to use var
+because of issue in blockscope and functional scope
+
+
+*/
+
+console.table([accountId,accountEmail,pass,accountCity,accountState])
+
